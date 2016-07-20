ab93e28f4760a4b3062cfb51a0252467d25a695e
Nicolas Petitprez <nicolas.petitprez@gmail.com>
2016-07-20 14:56:24 +0200

    update demo

--- src/main/java/com/restfully/shop/services/ShoppingApplication.java
+++ src/main/java/com/restfully/shop/services/ShoppingApplication.java
@@ -19,27 +19,20 @@ public class ShoppingApplication extends Application {
 
     public ShoppingApplication() {
 
-//        com.makitoo.MakitooJava.init(
-//                "https://dashboard.makitoo.com/rest",
-//                "testjaxrs-e5b9cad4-ae73-4147-b280-a793b40d313e",   // Application ID
-//                "cejgcf5putasgd4kb6ptnl7iu2",                     // Application token
-//                "0.0.1"                                           // Current version of your application
-//        );
-        Makitoo makitoo = com.makitoo.MakitooJava.init(
-                "http://ec2-52-27-189-237.us-west-2.compute.amazonaws.com/rest",
-                "testjaxrs-5d8a4667-5623-427e-bff6-955833b45677",   // Application ID
-                "knigco5168909rvomaegsk2qao",                     // Application token
-                "0.0.2"                                           // Current version of your application
+        Makitoo makitoo =   com.makitoo.MakitooJava.init(
+                "https://dashboard.makitoo.com/rest",
+                "testjaxrs-e5b9cad4-ae73-4147-b280-a793b40d313e",   // Application ID
+                "cejgcf5putasgd4kb6ptnl7iu2",                     // Application token
+                "0.0.6"                                           // Current version of your application
         );
 
-        ((Config)makitoo.getConfig()).recursiveContextSize.set(2);
-        ((Config)makitoo.getConfig()).contextSize.set(20);
+        ((Config) makitoo.getConfig()).recursiveContextSize.set(2);
+        ((Config) makitoo.getConfig()).contextSize.set(20);
 
 
         singletons.add(new CustomerResource());
         singletons.add(new OrderResource());
         singletons.add(new StoreResource());
-        singletons.add(new Bugs());
     }
 
     @Override
